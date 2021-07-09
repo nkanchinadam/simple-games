5a243cb22abbdd9dc08e9734c2c6878c014acd57
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2021-07-09 14:27:38 -0400

    misspelled onClick

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,7 +6,7 @@ function Square(props) {
   return (
     <button
       className="square"
-      onClic={props.onClick()}
+      onClick={props.onClick()}
     >
       {props.value}
     </button>
