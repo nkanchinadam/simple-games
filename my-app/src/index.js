d83c787bd7329d45240d79de6f36a6241a470074
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2021-07-09 15:00:49 -0400

    implemented taking turns between X and O

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -16,14 +16,18 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
     
 	handleClick(i) {
 		const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext
+    });
 	}
 
   renderSquare(i) {
