f23762f9b7b083a6199554a6b5814676224e2ac2
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2021-07-09 14:25:38 -0400

    turned the Square class into a function component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,19 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClic={props.onClick()}
+    >
+      {props.value}
+    </button>
+  );
 }
-  
 class Board extends React.Component {
   constructor(props) {
     super(props);
