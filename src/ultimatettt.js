47becab009323dc1dd3158c0ae4bf0a919a2a9cf
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2021-08-03 23:51:51 -0400

    added render functions to UltimateBoard & UltimateGame and added passing of onClick function down to UltimateBoard

--- src/ultimatettt.js
+++ src/ultimatettt.js
@@ -2,20 +2,51 @@ import React from 'react';
 import './index.css';
 import {Board, Game, calculateWinner, nullCheck} from './board.js';
 
+function calculateUltimateWinner(boards) {
+  reduced = Array(9);
+  for(let i = 0; i < reduced.length; i++) {
+    reduced[i] = calculateWinner(boards[i]);
+  }
+  return calculateWinner(reduced);
+}
+
 class UltimateBoard extends React.Component {
   renderBoard(i) {
     return <Board
-
+      squares={this.props.boards[i]}
+      onClick={(j) => this.props.onClick(i, j)}
     />
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderBoard(0)}
+          {this.renderBoard(1)}
+          {this.renderBoard(2)}
+        </div>
+        <div className="board-row">
+          {this.renderBoard(3)}
+          {this.renderBoard(4)}
+          {this.renderBoard(5)}
+        </div>
+        <div className="board-row">
+          {this.renderBoard(6)}
+          {this.renderBoard(7)}
+          {this.renderBoard(8)}
+        </div>
+      </div>
+    );
   }
 }
 
-export class UltimateGame extends Game {
+export class UltimateGame extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       history: [{
-        squares: Array(9).fill(Array(9).fill(null)),
+        boards: Array(9).fill(Array(9).fill(null)),
         nextBoard: null
       }],
       stepNumber: 0,
@@ -23,5 +54,29 @@ export class UltimateGame extends Game {
     }
   }
 
-  
+  handleClick(i, j) {
+
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateUltimateWinner(current.boards);
+
+
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <UltimateBoard
+            boards={current.boards}
+          />
+        </div>
+        <div className="game-info">
+          <div>{/*status*/}</div>
+          <ol>{/*moves*/}</ol>
+        </div>
+      </div>
+    );
+  }
 }
